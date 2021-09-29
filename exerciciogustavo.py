9079da542be231aee3f9fdfbafb94bf8492cdf38
HuanCarlos13 <201810297@acad.unijuazeiro.edu.br>
2021-09-29 20:08:16 -0300

    fist commit

---
+++ exerciciogustavo.py
@@ -0,0 +1,106 @@
+# # 1° questão 
+
+# # idade = int(input("Digite sua idade:"))
+
+# # if idade <16:
+# #     info = "Voto inválido!"
+# # elif idade < 18 or idade > 70:
+# #     info = "Voto opcional!"
+# # else:
+# #     info = "Voto obrigatório!"
+
+# 2° questão
+
+# # n1= int(input("Insira o primeiro número:"))
+# # n2= int(input("Insira o segundo número :"))
+# # n3= int(input("Insira o terceiro número:"))
+# # m = n1
+
+# #if n2 > m:
+# #     m = n2
+# #if n3 > m:
+# #     m = n3
+
+# # print(f"Maior numero é: {m}")
+
+# 3° questão 
+
+# # n1= int(input("Insira o primeiro número:"))
+# # n2= int(input("Insira o segundo número :"))
+# # n3= int(input("Insira o terceiro número:"))
+# # m = n1
+
+# #if n2 > m:
+# #     m = n2
+# #if n3 > m:
+# #     m = n3
+
+# # print(f"Maior numero é: {m}")
+
+# # menor = n1
+
+# #if n2 < menor:
+# #     menor = n2
+# #if n3 < menor:
+# #     menor = n3
+# # print(f"menor numero é: {menor}")
+
+# 4° 
+
+# # count = 0
+
+# # while count <= 100:
+# #     print(count)
+# #     count = count + 1
+
+# 5° 
+# # count = 0
+# # limite = int(input("Digite o limite:"))
+
+# # while count <= limite:
+# #     print(count)
+# #     count = count + 1
+
+# 6°
+# # count = 1
+# # nome = []
+
+# # while count <= 5:
+# #     nomes.append(input("Digite um nome:"))
+# #     count = count + 1
+
+# # print(f"Nomes: {nome}")
+
+# 7°
+# # while True:
+# #     n1 = int(input("Digite um número:"))
+# #     n2 = int(input("Digite outro número:"))
+# #     print(f"{n1} + {n2} = {n1 + n2}")
+    
+# #     resposta = input("Deseja realizar mais alguma soma? [S ou N]\n').upper()
+# #     print(f'Resposta: {resposta}")
+
+# #     if resposta == "S":
+# #         continue
+# #     if resposta == "N":
+# #         break
+
+# 8°
+
+# # index = 1
+# # funcionarios = []
+
+# # while index != 0:
+    
+# #funcionario = input(f'Funcionário {index}: ')
+# #index = index + 1
+
+# #if funcionario == '0':
+# #       break
+# #else:
+# #     funcionarios.append(funcionario)
+
+# # print(f"\nFuncionários: {funcionarios}")
+
+
+ 
